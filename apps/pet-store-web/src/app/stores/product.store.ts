e3b6b3f81375e322b836ff0fb36b32b52b858d83
XoDeR <xoder.vs@gmail.com>
2025-07-23 21:55:45 +0200

    products store

--- apps/pet-store-web/src/app/stores/product.store.ts
+++ apps/pet-store-web/src/app/stores/product.store.ts
@@ -1,16 +1,57 @@
-import { signalStore, withState } from '@ngrx/signals'
-import { Product } from '@prisma/client'
+import { inject } from '@angular/core'
+import { signalStore, withState, withMethods, patchState } from '@ngrx/signals'
+import type { Product } from '@prisma/client'
+import { Apollo, gql } from 'apollo-angular'
+import { tap } from 'rxjs'
+
+const GET_PRODUCTS = gql`
+  query GetProducts {
+    products {
+      id
+      name
+      description
+      price
+      image
+      stripePriceId
+    }
+  }
+`
 
 export interface ProductState {
-  products: Product
+  products: Product[],
+  featuredProducts: Product[],
+  loading: boolean,
+  error: string | null
 }
 
-const initialState = {
-  products: []
+const initialState: ProductState = {
+  products: [],
+  featuredProducts: [],
+  loading: false,
+  error: null
 }
 
 export const ProductStore = signalStore({
   providedIn: 'root'
 },
-withState()
+withState(initialState),
+withMethods((store, apollo = inject(Apollo)) => ({
+  loadProducts() {
+    patchState(store, {loading: true});
+    apollo.watchQuery<{products: Product[]}>({
+      query: GET_PRODUCTS
+    }).valueChanges.pipe(
+      tap({
+        next: ({data}) => patchState(
+          store,
+          {products: data.products, loading: false}
+        ),
+        error: (error) => patchState(
+          store,
+          {error: error.message, loading: false}
+        )
+      })
+    )
+  }
+}))
 ) 
