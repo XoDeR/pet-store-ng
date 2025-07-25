d63cee99a4d6844f8834c4b22cf4e658121d8c2e
XoDeR <xoder.vs@gmail.com>
2025-07-25 13:08:36 +0200

    Using debounce for input value

--- apps/pet-store-web/src/app/products/products.ts
+++ apps/pet-store-web/src/app/products/products.ts
@@ -1,18 +1,32 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { afterNextRender, Component, inject } from '@angular/core';
 import { ProductStore } from '../stores/product.store';
 import { ProductCard } from '../components/product-card/product-card';
+import { FormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule, ProductCard],
+  imports: [ProductCard, FormsModule],
   templateUrl: './products.html',
   styleUrl: './products.scss',
 })
 export class Products {
+  searchTerm = '';
   productStore = inject(ProductStore);
+  searchSubject = new Subject();
 
   constructor() {
     this.productStore.loadProducts();
+    afterNextRender(() => {
+      this.searchSubject
+        .pipe(debounceTime(500), distinctUntilChanged())
+        .subscribe((term) => {
+          console.log(term);
+        });
+    });
+  }
+
+  onSearch(term: string) {
+    this.searchSubject.next(term);
   }
 }
