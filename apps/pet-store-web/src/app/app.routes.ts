f3af1789641ea3e717453a9b86f984627b528633
XoDeR <xoder.vs@gmail.com>
2025-07-25 16:44:11 +0200

    added route for cart

--- apps/pet-store-web/src/app/app.routes.ts
+++ apps/pet-store-web/src/app/app.routes.ts
@@ -1,17 +1,26 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [{
-  path: '',
-  pathMatch: 'full',
-  loadComponent: async () => {
-    const mod = await import('./home/home');
-    return mod.Home;
-  }
-},
-{
-  path: 'products',
-  loadComponent: async () => {
-    const mod = await import('./products/products');
-    return mod.Products;
-  }
-}];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: async () => {
+      const mod = await import('./home/home');
+      return mod.Home;
+    },
+  },
+  {
+    path: 'products',
+    loadComponent: async () => {
+      const mod = await import('./products/products');
+      return mod.Products;
+    },
+  },
+  {
+    path: 'cart',
+    loadComponent: async () => {
+      const mod = await import('./cart/cart');
+      return mod.Cart;
+    },
+  },
+];
