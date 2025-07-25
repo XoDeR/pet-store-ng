c73b02d4bf36e22c60bd096381c90585dae2e20f
XoDeR <xoder.vs@gmail.com>
2025-07-25 23:44:50 +0200

    generated orders resource with GraphQL CRUD

--- apps/pet-store-be/src/app/app.module.ts
+++ apps/pet-store-be/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { ProductsModule } from './products/products.module';
 import { CheckoutModule } from './checkout/checkout.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { CheckoutModule } from './checkout/checkout.module';
     }),
     ProductsModule,
     CheckoutModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
