620140115857c6691ca1390989d95ff34fae38b2
XoDeR <xoder.vs@gmail.com>
2025-07-25 23:17:13 +0200

    generated checkout component

---
+++ apps/pet-store-web/src/app/checkout/checkout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-checkout',
+  imports: [CommonModule],
+  templateUrl: './checkout.html',
+  styleUrl: './checkout.scss',
+})
+export class Checkout {}
