28d11825d66a223988db94540da297a63eb02511
XoDeR <xoder.vs@gmail.com>
2025-07-25 23:42:07 +0200

    created checkout resource with REST CRUD

--- apps/pet-store-be/src/app/app.module.ts
+++ apps/pet-store-be/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { ProductsModule } from './products/products.module';
+import { CheckoutModule } from './checkout/checkout.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ProductsModule } from './products/products.module';
       autoSchemaFile: join(process.cwd(), 'apps/pet-store-be/dist/schema.gql'),
     }),
     ProductsModule,
+    CheckoutModule,
   ],
   controllers: [AppController],
   providers: [AppService],
