b39aa66fdebb95a2590dc353a4b46e50cf0f1a9e
XoDeR <xoder.vs@gmail.com>
2025-07-25 16:36:54 +0200

    cart component added, basic html template for cart

---
+++ apps/pet-store-web/src/app/cart/cart.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cart',
+  imports: [CommonModule],
+  templateUrl: './cart.html',
+  styleUrl: './cart.scss',
+})
+export class Cart {}
