ca77dd1a61c1691535a54ee971b5615ff3c8e567
XoDeR <xoder.vs@gmail.com>
2025-07-26 21:51:27 +0200

    checkout uses orders service from orders module

--- apps/pet-store-be/src/app/checkout/checkout.module.ts
+++ apps/pet-store-be/src/app/checkout/checkout.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CheckoutService } from './checkout.service';
 import { CheckoutController } from './checkout.controller';
+import { OrdersModule } from '../orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule],
   controllers: [CheckoutController],
   providers: [CheckoutService],
 })
