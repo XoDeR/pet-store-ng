fb356924f603b88af8ac3456d4d170f1917d8008
XoDeR <xoder.vs@gmail.com>
2025-07-27 22:36:11 +0200

    stripe service frontend

--- apps/pet-store-web/src/app/checkout/checkout.ts
+++ apps/pet-store-web/src/app/checkout/checkout.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartStore } from '../stores/cart.store';
+import { Stripe } from '../services/stripe';
 
 @Component({
   selector: 'app-checkout',
@@ -10,8 +11,11 @@ import { CartStore } from '../stores/cart.store';
 })
 export class Checkout {
   cartStore = inject(CartStore);
+  stripeService = inject(Stripe);
 
   checkout() {
-    console.log('Checkout');
+    this.stripeService.createCheckoutSession().subscribe(({ url }) => {
+      location.href = url;
+    });
   }
 }
