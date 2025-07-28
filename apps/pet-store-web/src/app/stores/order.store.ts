aab9801d4edefe2a13d0903861f3ee600e89d67a
XoDeR <xoder.vs@gmail.com>
2025-07-28 20:42:41 +0200

    added order store

---
+++ apps/pet-store-web/src/app/stores/order.store.ts
@@ -0,0 +1,71 @@
+import { inject } from '@angular/core';
+import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { Order, OrderItem, Product } from '@prisma/client';
+import { Apollo, gql } from 'apollo-angular';
+import { tap } from 'rxjs';
+
+const GET_ORDER = gql`
+  query GetOrder($id: String) {
+    order(id: $id) {
+      id
+      totalAmount
+      status
+      items {
+        id
+        quantity
+        price
+        product {
+          id
+          name
+          image
+        }
+      }
+      createdAt
+    }
+  }
+`;
+
+type OrderItemWithProduct = OrderItem & {
+  product: Product;
+};
+
+type OrderWithItems = Order & {
+  items: OrderItemWithProduct[];
+};
+
+type OrderState = {
+  orders: OrderWithItems[];
+  orderDetail: OrderWithItems | null;
+  error: string | null;
+};
+
+const initialState: OrderState = {
+  orders: [],
+  orderDetail: null,
+  error: null,
+};
+
+export const OrderStore = signalStore(
+  {
+    providedIn: 'root',
+  },
+  withState(() => initialState),
+  withMethods((store, apollo = inject(Apollo)) => ({
+    getOrder(id: string) {
+      patchState(store, { error: null });
+      return apollo
+        .query<{ order: OrderWithItems }>({
+          query: GET_ORDER,
+          variables: {
+            id,
+          },
+        })
+        .pipe(
+          tap({
+            next: ({ data }) => patchState(store, { orderDetail: data.order }),
+            error: (error) => patchState(store, { error: error.message }),
+          })
+        );
+    },
+  }))
+);
