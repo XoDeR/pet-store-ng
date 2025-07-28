64873b3b8a9d68bf7c552ac596468c9d221bdd4e
XoDeR <xoder.vs@gmail.com>
2025-07-28 21:22:49 +0200

    generated order-detail component

---
+++ apps/pet-store-web/src/app/orders/components/order-detail/order-detail.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-order-detail',
+  imports: [CommonModule],
+  templateUrl: './order-detail.html',
+  styleUrl: './order-detail.scss',
+})
+export class OrderDetail {}
