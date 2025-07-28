cedcb4115b45ca0a21e1f525986f32b032b11399
XoDeR <xoder.vs@gmail.com>
2025-07-28 20:04:02 +0200

    generated checkout-failure component

---
+++ apps/pet-store-web/src/app/checkout/checkout-failure/checkout-failure.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-checkout-failure',
+  imports: [CommonModule],
+  templateUrl: './checkout-failure.html',
+  styleUrl: './checkout-failure.scss',
+})
+export class CheckoutFailure {}
