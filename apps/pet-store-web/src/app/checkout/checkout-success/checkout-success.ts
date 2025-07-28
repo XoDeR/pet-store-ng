01eae76a85bd68958dd833a75088d4e8658a48e1
XoDeR <xoder.vs@gmail.com>
2025-07-28 20:05:16 +0200

    generated checkout-success component

---
+++ apps/pet-store-web/src/app/checkout/checkout-success/checkout-success.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-checkout-success',
+  imports: [CommonModule],
+  templateUrl: './checkout-success.html',
+  styleUrl: './checkout-success.scss',
+})
+export class CheckoutSuccess {}
