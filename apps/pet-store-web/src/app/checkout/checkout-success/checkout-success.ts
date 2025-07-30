b99670e040fed632b38d923a46589af13e74685d
XoDeR <xoder.vs@gmail.com>
2025-07-30 11:14:10 +0200

    clear cart after successful checkout

--- apps/pet-store-web/src/app/checkout/checkout-success/checkout-success.ts
+++ apps/pet-store-web/src/app/checkout/checkout-success/checkout-success.ts
@@ -1,8 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { afterNextRender, Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { OrderStore } from '../../stores/order.store';
 import { OrderDetail } from '../../orders/components/order-detail/order-detail';
+import { CartStore } from '../../stores/cart.store';
 
 @Component({
   selector: 'app-checkout-success',
@@ -13,6 +14,13 @@ import { OrderDetail } from '../../orders/components/order-detail/order-detail';
 export class CheckoutSuccess implements OnInit {
   orderStore = inject(OrderStore);
   route = inject(ActivatedRoute);
+  cartStore = inject(CartStore);
+
+  constructor() {
+    afterNextRender(() => {
+      this.cartStore.clearCart();
+    });
+  }
 
   ngOnInit() {
     const orderId = this.route.snapshot.queryParamMap.get('orderId');
