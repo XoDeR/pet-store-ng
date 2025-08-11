476172b911a770eb5526928d96c142c913ffd139
XoDeR <xoder.vs@gmail.com>
2025-08-11 18:05:26 +0200

    home page initial (wip)

--- apps/pet-store-web/src/app/home/home.ts
+++ apps/pet-store-web/src/app/home/home.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ProductCard } from '../components/product-card/product-card';
+import { RouterLink } from '@angular/router';
+import { Product } from '@prisma/client';
+import { ProductStore } from '../stores/product.store';
+import { CartStore } from '../stores/cart.store';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCard, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  productStore = inject(ProductStore);
+  cartStore = inject(CartStore);
+
+  constructor() {
+    // this.productStore.loadFeaturedProducts();
+  }
+
+  onAddToCart(product: Product) {
+    this.cartStore.addToCart(product);
+  }
+}
