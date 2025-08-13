ea4e8dfdbc08bd6c73c68d9d388b94f66f8c28ca
XoDeR <xoder.vs@gmail.com>
2025-08-13 21:58:25 +0200

    auth functions with firebase auth implemented

--- apps/pet-store-web/src/app/auth/auth.ts
+++ apps/pet-store-web/src/app/auth/auth.ts
@@ -1,8 +1,64 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from '@angular/fire/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class Auth {
-  
+export class AuthService {
+  auth = inject(Auth);
+
+  async login(email: string, password: string) {
+    try {
+      const result = await signInWithEmailAndPassword(
+        this.auth,
+        email,
+        password
+      );
+      return result.user;
+    } catch (error) {
+      console.error('Login error: ', error);
+      throw error;
+    }
+  }
+
+  async signup(email: string, password: string) {
+    try {
+      const result = await createUserWithEmailAndPassword(
+        this.auth,
+        email,
+        password
+      );
+      return result.user;
+    } catch (error) {
+      console.error('Signup error: ', error);
+      throw error;
+    }
+  }
+
+  async googleSignIn() {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(this.auth, provider);
+      return result.user;
+    } catch (error) {
+      console.error('Google signin error: ', error);
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      await signOut(this.auth);
+    } catch (error) {
+      console.error('Logout error: ', error);
+      throw error;
+    }
+  }
 }
