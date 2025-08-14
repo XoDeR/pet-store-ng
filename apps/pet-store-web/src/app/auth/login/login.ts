1209ad7bfef71b2dc66df3bc7c282d174a4e5fba
XoDeR <xoder.vs@gmail.com>
2025-08-14 11:11:17 +0200

    login, signup completed

--- apps/pet-store-web/src/app/auth/login/login.ts
+++ apps/pet-store-web/src/app/auth/login/login.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../auth';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  email = '';
+  password = '';
+
+  async onSubmit() {
+    try {
+      await this.auth.login(this.email, this.password);
+      this.router.navigate(['/']);
+    } catch (error) {
+      console.error('Login error:', error);
+    }
+  }
+
+  async loginWithGoogle() {
+    try {
+      await this.auth.googleSignIn();
+      this.router.navigate(['/']);
+    } catch (error) {
+      console.error('Google login error:', error);
+    }
+  }
+}
--- apps/pet-store-web/src/app/auth/signup/signup.ts
+++ apps/pet-store-web/src/app/auth/signup/signup.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../auth';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
-export class Signup {}
+export class Signup {
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  email = '';
+  password = '';
+
+  async onSubmit() {
+    try {
+      await this.auth.signup(this.email, this.password);
+      this.router.navigate(['/']);
+    } catch (error) {
+      console.error('Signup error:', error);
+    }
+  }
+
+  async signupWithGoogle() {
+    try {
+      await this.auth.googleSignIn();
+      this.router.navigate(['/']);
+    } catch (error) {
+      console.error('Google signup error:', error);
+    }
+  }
+}
