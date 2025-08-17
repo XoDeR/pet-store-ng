42ccaf73f31637e4b445ce5586c1ee3074f3b4aa
XoDeR <xoder.vs@gmail.com>
2025-08-17 21:54:07 +0200

    added firebase service for server side

---
+++ apps/pet-store-be/src/app/firebase/firebase.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+
+@Injectable()
+export class FirebaseService implements OnModuleInit {
+  onModuleInit() {
+    if (!admin.apps.length) {
+      admin.initializeApp({
+        credential: admin.credential.cert(
+          //
+          null,
+          null
+        ),
+      });
+    }
+  }
+}
